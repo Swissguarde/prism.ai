6211268a361a15ff3e808e1cbbafff7fe71750a6
Kolade Ayomide <ayomikolade2003@gmail.com>
2024-04-12 18:02:03 +0100

    added features page

---
+++ app/features/page.tsx
@@ -0,0 +1,9 @@
+export default function FeaturesPage() {
+  return (
+    <div className="flex items-center justify-center py-40">
+      <h2 className="text-balance font-semibold tracking-wider sm:text-4xl">
+        Cool features
+      </h2>
+    </div>
+  );
+}
